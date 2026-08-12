f92c250b05b4449ad6be54ee6ee87a9f17b02c63
Adrija <adrijaghosh5002@gmail.com>
2026-08-12 17:25:35 +0530

    Fix production socket connection

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://snapwave-bha7.onrender.com", {
   autoConnect: false,
   transports: ["websocket"],
 });
